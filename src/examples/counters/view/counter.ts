74e090951f5a3cb7a3adcd19e8c8886216825bd9
Alon Bar <alon.r.bar@gmail.com>
2017-10-21 02:42:54 +0300

    updated code note

--- src/examples/counters/view/counter.ts
+++ src/examples/counters/view/counter.ts
@@ -5,16 +5,17 @@ import { Counter } from "../model/counter";
 export class CounterElement {
 
     //
-    // Note: 
+    // Advanced Note: 
     //
-    // Binding each part of the component separately is not recommended as some
-    // advanced features of redux-app won't work. It's done here for to show one
-    // possible usage pattern, you can find a more recommended usage pattern in
-    // the other examples.
+    // In Aurelia, methods and callbacks bound to @bindable properties are
+    // executed with the receiving element used as their 'this' argument. This
+    // poses a problem to some advanced features of redux-app such as the withId
+    // decorator. There are several solutions to this and you can find one
+    // recommended usage pattern in the other examples.
     //
     // Also note that for the reasons portrayed above ReduxApp will emit a
-    // warning about situations like this when setting ReduxApp.options.logLevel to
-    // LogLevel.Debug or above.
+    // warning about situations like this when setting ReduxApp.options.logLevel
+    // to LogLevel.Debug or above.
     //
 
     @bindable public counter: Counter;    
