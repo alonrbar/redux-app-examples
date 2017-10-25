f0878915e80d1cc320ef88bdefaa1452a72f0abb
Alon Bar <alon.r.bar@gmail.com>
2017-10-25 15:36:47 +0300

    updated example

--- src/examples/counters/view/counter.ts
+++ src/examples/counters/view/counter.ts
@@ -1,23 +1,7 @@
 import { customElement, bindable } from "aurelia-framework";
-import { Counter } from "../model/counter";
+import { CounterComponent } from "../viewModel/counter";
 
 @customElement('counter')
 export class CounterElement {
-
-    //
-    // Advanced Note: 
-    //
-    // In Aurelia, methods and callbacks bound to @bindable properties are
-    // executed with the receiving element used as their 'this' argument. This
-    // poses a problem to some advanced features of redux-app such as the withId
-    // decorator. There are several solutions to this and you can find one
-    // recommended usage pattern in the other examples.
-    //
-    // Also note that for the reasons portrayed above ReduxApp will emit a
-    // warning about situations like this when setting ReduxApp.options.logLevel
-    // to LogLevel.Debug or above.
-    //
-
-    @bindable public counter: Counter;    
-    @bindable public increment: () => void;
+    @bindable public vm: CounterComponent;
 }
--- src/examples/counters/viewModel/counter.ts
+++ src/examples/counters/viewModel/counter.ts
@@ -1,21 +1,18 @@
 import { component } from 'redux-app';
-import { Counter } from '../model/counter';
 
 @component
 export class CounterComponent {
 
-    public counter = new Counter();
+    private static countersId = 1;
+
+    public id = CounterComponent.countersId++;
+    public value = 0;
 
     public increment(counterId: number): void {
 
-        if (counterId !== this.counter.id)
+        if (counterId !== this.id)
             return;
 
-        const { value, ...rest } = this.counter;
-
-        this.counter = {
-            value: value + 1,
-            ...rest
-        };
+        this.value = this.value + 1;
     }
 }
